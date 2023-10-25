879f4149c5b3e303168ebd815a2912d36eb23dfa
Amir <amirgha7@gmail.com>
2023-10-25 16:06:59 +0200

    Chat gpt changes

--- flask3 copy/flask-mvc-example/project/controllers/testing.py
+++ flask3 copy/flask-mvc-example/project/controllers/testing.py
@@ -51,37 +51,133 @@ def delete_car_info():
 
 
 #CHAT GPT
+# 1, 
+@app.route('/save_car', methods=["POST"]) 
+def save_car_info():
+    make = request.form["make"]
+    model = request.form["model"]
+    reg = request.form["reg"]
+    year = request.form["year"]
+    capacity = request.form["capacity"]
+    location = request.form["location"]  # Get location from the form
+    status = request.form.get("status", "available")  # Get status from the form, default is available
+    return save_car(make, model, reg, year, capacity, location, status)
 
-#Endpoint ordering a car
-@app.route('/order-car', methods=['POST'])
-def order_car():
-    data = request.json
-    customer_id = data['customer_id']
-    car_id = data['car_id']
+@app.route('/update_car', methods=['PUT']) 
+def update_car_info():
+    record = json.loads(request.data)
+    return update_car(
+        record['make'], record['model'], record['reg'], 
+        record['year'], record['capacity'], record['location'], record['status']
+    )
 
-    # Check if the customer has not booked other cars.
-    booked_car = find_booked_car_for_customer(customer_id)
-    if booked_car:
-        return jsonify({"error": "Customer already has a booking!"}), 400
+#2. CRUD for "Customers"
+@app.route('/save_customer', methods=["POST"]) 
+def save_customer_info():
+    name = request.form["name"]
+    age = request.form["age"]
+    address = request.form["address"]
+    return save_customer(name, age, address)
 
-    # Change the status of the car to 'booked'
-    book_car_for_customer(customer_id, car_id)
-    return jsonify({"message": "Car booked successfully!"})
+@app.route('/get_all_customers', methods=['GET']) 
+def get_all_customers():
+    return find_all_customers()
 
-#Endpoint cancel order
-@app.route('/cancel-order-car', methods=['POST'])
-def cancel_order_car():
-    data = request.json
-    customer_id = data['customer_id']
-    car_id = data['car_id']
+@app.route('/get_customer_by_name', methods=['POST'])
+def get_customer_by_name():
+    record = json.loads(request.data)
+    return find_customer_by_name(record['name'])
 
-    # Check if the customer has booked the specific car.
-    booked_car = find_specific_booked_car_for_customer(customer_id, car_id)
-    if not booked_car:
-        return jsonify({"error": "No booking found for this customer and car!"}), 400
+@app.route('/update_customer', methods=['PUT']) 
+def update_customer_info():
+    record = json.loads(request.data)
+    name = record['name']
+    age = record.get('age')
+    address = record.get('address')
+    return update_customer(name, age, address)
 
-    # Cancel the booking
-    cancel_booking_for_car_and_customer(customer_id, car_id)
-    return jsonify({"message": "Booking canceled successfully!"})
+@app.route('/delete_customer', methods=['DELETE'])
+def delete_customer_info():
+    record = json.loads(request.data)
+    delete_customer(record['name'])
+    return {"message": "Customer deleted successfully!"}
 
+#3. CRUD for "Employee" class
 
+@app.route('/save_employee', methods=["POST"]) 
+def save_employee_info():
+    name = request.form["name"]
+    address = request.form["address"]
+    branch = request.form["branch"]
+    return save_employee(name, address, branch)
+
+@app.route('/get_all_employees', methods=['GET']) 
+def get_all_employees():
+    return find_all_employees()
+
+@app.route('/get_employee_by_name', methods=['POST'])
+def get_employee_by_name():
+    record = json.loads(request.data)
+    return find_employee_by_name(record['name'])
+
+@app.route('/update_employee', methods=['PUT']) 
+def update_employee_info():
+    record = json.loads(request.data)
+    name = record['name']
+    address = record.get('address')
+    branch = record.get('branch')
+    return update_employee(name, address, branch)
+
+@app.route('/delete_employee', methods=['DELETE'])
+def delete_employee_info():
+    record = json.loads(request.data)
+    delete_employee(record['name'])
+    return {"message": "Employee deleted successfully!"}
+
+#4/5. Order_car implementation
+
+@app.route('/order-car', methods=["POST"]) 
+def order_car_endpoint():
+    try:
+        record = json.loads(request.data)
+        customer_id = int(record["customer_id"])
+        car_id = int(record["car_id"])
+        return order_car(customer_id, car_id)
+    except Exception as e:
+        return {"error": str(e)}, 400
+
+# 6. Cancel_order_car endpoint
+@app.route('/cancel-order-car', methods=["POST"]) 
+def cancel_order_car_endpoint():
+    try:
+        record = json.loads(request.data)
+        customer_id = int(record["customer_id"])
+        car_id = int(record["car_id"])
+        return cancel_order_car(customer_id, car_id)
+    except Exception as e:
+        return {"error": str(e)}, 400
+
+#7. rent-car function
+
+@app.route('/rent-car', methods=["POST"]) 
+def rent_car_endpoint():
+    try:
+        record = json.loads(request.data)
+        customer_id = int(record["customer_id"])
+        car_id = int(record["car_id"])
+        return rent_car(customer_id, car_id)
+    except Exception as e:
+        return {"error": str(e)}, 400
+
+# 8. Return-car function
+
+@app.route('/return-car', methods=["POST"]) 
+def return_car_endpoint():
+    try:
+        record = json.loads(request.data)
+        customer_id = int(record["customer_id"])
+        car_id = int(record["car_id"])
+        return_status = record["status"]
+        return return_car(customer_id, car_id, return_status)
+    except Exception as e:
+        return {"error": str(e)}, 400
--- flask3 copy/flask-mvc-example/project/models/my_dao.py
+++ flask3 copy/flask-mvc-example/project/models/my_dao.py
@@ -27,53 +27,241 @@ def findCarByReg(reg):
      nodes_json = [node_to_json(record["a"]) for record in cars]
      print(nodes_json)
      return nodes_json
-  
+
+
+#CHAT GPT
+ 
+#1. CRUD for "Cars" class  
+#We have added status to the function
 def save_car(make, model, reg, year, capacity):
-    cars = _get_connection().execute_query("MERGE (a:Car{make: $make, model: $model,reg: $reg, year: $year, capacity:$capacity}) RETURN a;", make = make, model = model, reg = reg, year = year, capacity = capacity)
-    nodes_json = [node_to_json(record["a"]) for record in cars]
-    print(nodes_json)
-    return nodes_json
+  cars = _get_connection().execute_query(
+          "MERGE (a:Car{make: $make, model: $model, reg: $reg, year: $year, capacity: $capacity, location: $location, status: $status}) RETURN a;",
+          make=make, model=model, reg=reg, year=year, capacity=capacity, location=location, status=status
+      )
+  nodes_json = [node_to_json(record["a"]) for record in cars]
+  return nodes_json
 
 def delete_car(reg):
   _get_connection().execute_query("MATCH (a:Car{reg: $reg}) delete a;", reg = reg)
 
+#
+def update_car(make, model, reg, year, capacity, location, status):
+  with _get_connection().session() as session:
+      car = session.run(
+            """
+            MATCH (a:Car{reg: $reg}) 
+            SET a.make = $make, a.model = $model, a.year = $year, a.capacity = $capacity, a.location = $location, a.status = $status
+            RETURN a;
+            """,
+            make=make, model=model, reg=reg, year=year, capacity=capacity, location=location, status=status
+        )
+      nodes_json = [node_to_json(record["a"]) for record in car]
+      return nodes_json
 
 
+#2. CRUD for "Customer" class
+def save_customer(name, age, address):
+    with _get_connection().session() as session:
+        customers = session.run(
+            """
+            MERGE (c:Customer{name: $name, age: $age, address: $address})
+            RETURN c;
+            """,
+            name=name, age=age, address=address
+        )
+        nodes_json = [node_to_json(record["c"]) for record in customers]
+        return nodes_json
+
+def find_all_customers():
+    with _get_connection().session() as session:
+        customers = session.run("MATCH (c:Customer) RETURN c;")
+        nodes_json = [node_to_json(record["c"]) for record in customers]
+        return nodes_json
+
+def find_customer_by_name(name):
+    with _get_connection().session() as session:
+        customers = session.run("MATCH (c:Customer) WHERE c.name = $name RETURN c;", name=name)
+        nodes_json = [node_to_json(record["c"]) for record in customers]
+        return nodes_json
+
+def update_customer(name, age=None, address=None):
+    with _get_connection().session() as session:
+        query = "MATCH (c:Customer{name: $name}) "
+        if age:
+            query += "SET c.age = $age "
+        if address:
+            query += "SET c.address = $address "
+        query += "RETURN c;"
+        customer = session.run(query, name=name, age=age, address=address)
+        nodes_json = [node_to_json(record["c"]) for record in customer]
+        return nodes_json
+
+def delete_customer(name):
+    with _get_connection().session() as session:
+        session.run("MATCH (c:Customer{name: $name}) DELETE c;", name=name)
+
+#3. CRUD for "Employee" class
+def save_employee(name, address, branch):
+    with _get_connection().session() as session:
+        employees = session.run(
+            """
+            MERGE (e:Employee{name: $name, address: $address, branch: $branch})
+            RETURN e;
+            """,
+            name=name, address=address, branch=branch
+        )
+        nodes_json = [node_to_json(record["e"]) for record in employees]
+        return nodes_json
+
+def find_all_employees():
+    with _get_connection().session() as session:
+        employees = session.run("MATCH (e:Employee) RETURN e;")
+        nodes_json = [node_to_json(record["e"]) for record in employees]
+        return nodes_json
+
+def find_employee_by_name(name):
+    with _get_connection().session() as session:
+        employees = session.run("MATCH (e:Employee) WHERE e.name = $name RETURN e;", name=name)
+        nodes_json = [node_to_json(record["e"]) for record in employees]
+        return nodes_json
+
+def update_employee(name, address=None, branch=None):
+    with _get_connection().session() as session:
+        query = "MATCH (e:Employee{name: $name}) "
+        if address:
+            query += "SET e.address = $address "
+        if branch:
+            query += "SET e.branch = $branch "
+        query += "RETURN e;"
+        employee = session.run(query, name=name, address=address, branch=branch)
+        nodes_json = [node_to_json(record["e"]) for record in employee]
+        return nodes_json
+
+def delete_employee(name):
+    with _get_connection().session() as session:
+        session.run("MATCH (e:Employee{name: $name}) DELETE e;", name=name)
 
 
+#4/5. Order_car implementation and checking whether the customer already has rented a car
 
-#CHATGPT
+def order_car(customer_id, car_id):
+    with _get_connection().session() as session:
+        # Check if the car is already rented or booked
+        rented_or_booked_car = session.run(
+            "MATCH (c:Car) WHERE ID(c) = $car_id AND (c.status = 'booked' OR EXISTS((:Customer)-[:RENTS]->(c))) RETURN c",
+            car_id=car_id
+        ).single()
+        if rented_or_booked_car:
+            raise Exception("Car is already rented or booked!")
+        
+        # Check if the customer already has a rented or booked car
+        customer_car = session.run(
+            "MATCH (c:Customer) WHERE ID(c) = $customer_id AND EXISTS((c)-[:RENTS]->(:Car)) RETURN c",
+            customer_id=customer_id
+        ).single()
+        if customer_car:
+            raise Exception("Customer already has a rented or booked car!")
+        
+        # Create the RENTS relationship and update the car status
+        result = session.run(
+            """
+            MATCH (c:Customer), (a:Car)
+            WHERE ID(c) = $customer_id AND ID(a) = $car_id
+            CREATE (c)-[:RENTS]->(a)
+            SET a.status = 'booked'
+            RETURN a;
+            """,
+            customer_id=customer_id, car_id=car_id
+        )
+        
+        nodes_json = [node_to_json(record["a"]) for record in result]
+        return nodes_json
 
-# CRUD for Cars
-@app.route('/create_car', methods=['POST'])
-def create_car():
-    data = request.json
-    make = data["make"]
-    model = data["model"]
-    year = data["year"]
-    location = data["location"]
-    status = "available"
-    save_car(make, model, year, location, status, car_id)
-    return jsonify({"message": "Car created successfully!"})
+# 6. Cancel_order_car endpoint
 
-#CRUD for customer
-@app.route('/create_customer', methods=['POST'])
-def create_customer():
-    data = request.json
-    name = data["name"]
-    age = data["age"]
-    address = data["address"]
-    customer_id = str(uuid.uuid4())  # Generate a unique ID
-    save_customer(name, age, address, customer_id)
-    return jsonify({"message": "Customer created successfully!"})
+def cancel_order_car(customer_id, car_id):
+    with _get_connection().session() as session:
+        # Check if the customer has booked this car
+        booking_exists = session.run(
+            "MATCH (c:Customer)-[r:RENTS]->(a:Car) WHERE ID(c) = $customer_id AND ID(a) = $car_id RETURN r",
+            customer_id=customer_id, car_id=car_id
+        ).single()
 
-#CRUD for employee
-@app.route('/create_employee', methods=['POST'])
-def create_employee():
-    data = request.json
-    name = data["name"]
-    address = data["address"]
-    branch = data["branch"]
-    employee_id = str(uuid.uuid4())  # Generate a unique ID
-    save_employee(name, address, branch, employee_id)
-    return jsonify({"message": "Employee created successfully!"})
+        if not booking_exists:
+            raise Exception("No booking found for this customer with this car!")
+
+        # Remove the RENTS relationship and update the car status
+        result = session.run(
+            """
+            MATCH (c:Customer)-[r:RENTS]->(a:Car)
+            WHERE ID(c) = $customer_id AND ID(a) = $car_id
+            DELETE r
+            SET a.status = 'available'
+            RETURN a;
+            """,
+            customer_id=customer_id, car_id=car_id
+        )
+        
+        nodes_json = [node_to_json(record["a"]) for record in result]
+        return nodes_json
+
+#7. rent-car function
+
+def rent_car(customer_id, car_id):
+    with _get_connection().session() as session:
+        # Check if the customer has booked this car and it's in 'booked' status
+        booking_exists = session.run(
+            "MATCH (c:Customer)-[:RENTS]->(a:Car) WHERE ID(c) = $customer_id AND ID(a) = $car_id AND a.status = 'booked' RETURN a",
+            customer_id=customer_id, car_id=car_id
+        ).single()
+
+        if not booking_exists:
+            raise Exception("No valid booking found for this customer with this car!")
+
+        # Update the car status to 'rented'
+        result = session.run(
+            """
+            MATCH (c:Customer)-[:RENTS]->(a:Car)
+            WHERE ID(c) = $customer_id AND ID(a) = $car_id
+            SET a.status = 'rented'
+            RETURN a;
+            """,
+            customer_id=customer_id, car_id=car_id
+        )
+        
+        nodes_json = [node_to_json(record["a"]) for record in result]
+        return nodes_json
+
+# 8. Return-car function
+
+def return_car(customer_id, car_id, return_status):
+    with _get_connection().session() as session:
+        # Validate the return_status
+        if return_status not in ['available', 'damaged']:
+            raise ValueError("Invalid return status provided!")
+
+        # Check if the customer has rented this car and it's in 'rented' status
+        renting_exists = session.run(
+            "MATCH (c:Customer)-[:RENTS]->(a:Car) WHERE ID(c) = $customer_id AND ID(a) = $car_id AND a.status = 'rented' RETURN a",
+            customer_id=customer_id, car_id=car_id
+        ).single()
+
+        if not renting_exists:
+            raise Exception("No valid renting found for this customer with this car!")
+
+        # Update the car status based on the return_status
+        result = session.run(
+            """
+            MATCH (c:Customer)-[:RENTS]->(a:Car)
+            WHERE ID(c) = $customer_id AND ID(a) = $car_id
+            SET a.status = $return_status
+            DELETE (c)-[:RENTS]->(a)
+            RETURN a;
+            """,
+            customer_id=customer_id, car_id=car_id, return_status=return_status
+        )
+        
+        nodes_json = [node_to_json(record["a"]) for record in result]
+        return nodes_json
+
+#KJØR KODENE I POSTMAN OG OM ALT ER RIKTIG TRENGER VI BARE Å LAGE REPORT OG SETTE TING I GITHUB
