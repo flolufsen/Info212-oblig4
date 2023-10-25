bb7a21fbe4b99bac8927a987273a203317a05d10
Amir <amirgha7@gmail.com>
2023-10-25 11:22:33 +0200

    blbalbalabal

--- flask3 copy/flask-mvc-example/project/controllers/testing.py
+++ flask3 copy/flask-mvc-example/project/controllers/testing.py
@@ -5,3 +5,13 @@ from project.models.my_dao import *
 
 def query_records():
     return findAllCars()
+
+
+ # The method uses the registration number to find the car
+  # object from database
+@app.route('/get_cars_by_reg_number', methods=['POST'])
+def find_car_by_reg_number():
+    record = json.loads(request.data)
+    print(record)
+    print(record['reg'])
+    return findCarByReg(record['reg'])
