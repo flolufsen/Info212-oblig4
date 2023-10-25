2d0a89c67f71b725d8816765e31068d5092f9cd7
flolufsen <144833870+flolufsen@users.noreply.github.com>
2023-10-25 10:28:00 +0200

    added flask3

---
+++ flask3 copy/flask-mvc-example/project/models/User.py
@@ -0,0 +1,82 @@
+from neo4j import GraphDatabase, Driver, AsyncGraphDatabase, AsyncDriver
+import re
+
+URI = "neo4j+s://dfb8c54f.databases.neo4j.io"
+AUTH = ("neo4j", "roTGhF0Rz9ifMS0J7oPW7urmchWg9PlZU76-X3o8xJU")
+
+
+def _get_connection() -> Driver:
+    driver = GraphDatabase.driver(URI, auth=AUTH)
+    driver.verify_connectivity()
+
+    return driver
+
+def findUserByName(name):
+    data = _get_connection().execute_query("MATCH (a:User) where a.name = $name RETURN a;", name=name)
+    if len(data[0]) > 0:
+        user = Costumer(name, data[0][0][0]['age'])
+        return user
+    else:
+        return Costumer(name, "Not found in DB")
+
+class Costumer:
+    def __init__(self, name, age, address):
+        self.name = name
+        self.age = age
+        self.address = address
+
+    def get_Name(self):
+        return self.name
+
+    def set_Name(self, value):
+        self.name = value
+
+    def get_Age(self):
+        return self.age
+
+    def set_Age(self, value):
+        self.email = value
+    
+    def get_Address(self):
+        return self.address
+
+    def set_Address(self, value):
+        self.address = value
+
+class Car:
+    def __init__(self, make, model, year, location, status):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.location = location
+        self.status = status
+
+    def get_Make(self):
+        return self.make
+
+    def set_Make(self, value):
+        self.make = value
+
+    def get_Model(self):
+        return self.model
+
+    def set_Model(self, value):
+        self.model = value
+    
+    def get_Year(self):
+        return self.year
+
+    def set_Year(self, value):
+        self.year = value
+
+    def get_Locatiion(self):
+        return self.location
+
+    def set_Location(self, value):
+        self.location = value
+
+    def get_Status(self):
+        return self.status
+
+    def set_Status(self, value):
+        self.status = value
