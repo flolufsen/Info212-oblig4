0a969854b01d764a587528fe7d18147220d5b07b
Amir <amirgha7@gmail.com>
2023-10-25 11:32:23 +0200

    fdfdf

--- flask3 copy/flask-mvc-example/project/controllers/testing.py
+++ flask3 copy/flask-mvc-example/project/controllers/testing.py
@@ -28,3 +28,21 @@ def save_car_info():
 
 
 
+  # The method uses the registration number to find the car
+# object from database and updates other informaiton from
+# the information provided as input in the json object
+@app.route('/update_car', methods=['PUT']) 
+def update_car_info():
+    record = json.loads(request.data)
+    print(record)
+    return update_car(record['make'], record['model'], 
+    record['reg'], record['year'], record['capacity'])
+
+  # The method uses the registration number to find the car
+  # object from database and removes the records
+@app.route('/delete_car', methods=['DELETE'])
+def delete_car_info():
+    record = json.loads(request.data)
+    print(record)
+    delete_car(record['reg'])
+    return findAllCars()
