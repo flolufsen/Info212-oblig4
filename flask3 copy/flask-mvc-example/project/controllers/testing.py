2dbea3ebb3bef05b7aed7d975e6b52aa3c4e26de
Amir <amirgha7@gmail.com>
2023-10-25 11:49:03 +0200

    kkk

--- flask3 copy/flask-mvc-example/project/controllers/testing.py
+++ flask3 copy/flask-mvc-example/project/controllers/testing.py
@@ -1,3 +1,5 @@
+
+
 from project import app
 from flask import render_template, request, redirect, url_for
 from project.models.my_dao import *
@@ -46,3 +48,58 @@ def delete_car_info():
     print(record)
     delete_car(record['reg'])
     return findAllCars()
+
+
+
+
+#CHAT GPT
+
+@app.route('/customer', methods=["POST"])
+def create_customer_endpoint():
+    name = request.form["name"]
+    age = request.form["age"]
+    address = request.form["address"]
+    create_customer(name, age, address)
+    return jsonify({"message": "Customer created!"})
+
+@app.route('/customer', methods=["GET"])
+def get_customers_endpoint():
+    return jsonify(get_customers())
+
+@app.route('/customer/<customer_id>', methods=["PUT"])
+def update_customer_endpoint(customer_id):
+    name = request.form["name"]
+    age = request.form["age"]
+    address = request.form["address"]
+    update_customer(customer_id, name, age, address)
+    return jsonify({"message": "Customer updated!"})
+
+@app.route('/customer/<customer_id>', methods=["DELETE"])
+def delete_customer_endpoint(customer_id):
+    delete_customer(customer_id)
+    return jsonify({"message": "Customer deleted!"})
+
+@app.route('/employee', methods=["POST"])
+def create_employee_endpoint():
+    name = request.form["name"]
+    address = request.form["address"]
+    branch = request.form["branch"]
+    create_employee(name, address, branch)
+    return jsonify({"message": "Employee created!"})
+
+@app.route('/employee', methods=["GET"])
+def get_employees_endpoint():
+    return jsonify(get_employees())
+
+@app.route('/employee/<employee_id>', methods=["PUT"])
+def update_employee_endpoint(employee_id):
+    name = request.form["name"]
+    address = request.form["address"]
+    branch = request.form["branch"]
+    update_employee(employee_id, name, address, branch)
+    return jsonify({"message": "Employee updated!"})
+
+@app.route('/employee/<employee_id>', methods=["DELETE"])
+def delete_employee_endpoint(employee_id):
+    delete_employee(employee_id)
+    return jsonify({"message": "Employee deleted!"})
